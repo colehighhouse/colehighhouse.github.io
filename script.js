bbf2cf1b94b9c4941974c9ed3b6ad6e1e0b02041
colehighhouse <63878596+colehighhouse@users.noreply.github.com>
2023-09-06 19:39:28 +0800

    box

--- script.js
+++ script.js
@@ -17,22 +17,3 @@ document.addEventListener("mouseup", () => {
   document.removeEventListener("mousemove", move);
 });
 
-
-const div = document.querySelector("featurepost");
-let offsetX, offsetY; 
-
-const move = (e) => {
-  div.style.left = `${e.clientX - offsetX}px`;
-  div.style.top = `${e.clientY - offsetY}px`;
-}
-
-div.addEventListener("mousedown", (e) => {
-  offsetX = e.clientX - div.offsetLeft;
-  offsetY = e.clientY - div.offsetTop;
-  document.addEventListener("mousemove", move);
-
-})
-
-document.addEventListener("mouseup", () => {
-  document.removeEventListener("mousemove", move);
-});
